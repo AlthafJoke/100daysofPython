94c56057fe25af07214b0a087a0ba90969b643cc
Althafav <61247025+althafav@users.noreply.github.com>
2023-04-07 15:12:57 +0530

    added photography charge with ticket counter

--- ticket_counter.py
+++ ticket_counter.py
@@ -6,17 +6,35 @@ height_limit = 120
 adult = 18
 adult_cost = 120
 child_cost = 60
+photography_charge = 20
 
 
 
 if height >= height_limit:
     print("You can ride the rollercoaster")
     age = int(input("what is your age ?: "))
-    if age >= adult:
-        print(f"you need to pay {adult_cost}")
+    photography = (input("Do you want photography? yes or no ? :")).lower()
     
+    if photography == 'yes':
+        photography = True
+    elif photography == 'no':
+        photography = False
     else:
-        print(f"you need to pay {child_cost}")
+        print("please provide valid input")
+      
+    
+    if age >= adult:
+        if photography:
+            print(f"you need to pay {adult_cost + photography_charge}")
+        else:
+            print(f"you need to pay {adult_cost}")
+        
+    else:
+        if photography:
+            print(f"you need to pay {child_cost + photography_charge}")
+        
+        else:
+            print(f"you need to pay {child_cost}")
 
 else:
     print("sorry, you can't ride the rollercoaster")
