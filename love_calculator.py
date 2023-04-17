997c4cbdf753364603ba6109876c6b9e994182de
Althafav <61247025+althafav@users.noreply.github.com>
2023-04-17 13:26:36 +0530

    love calculator added

---
+++ love_calculator.py
@@ -0,0 +1,38 @@
+name = input("Enter your name:")
+name2 = input("Enter your lover name: ")
+
+name = (name + name2).lower()
+
+value = "true"
+value2 = "love"
+
+
+def love_calculator(name, value1, value2):
+    num1 = 0
+    num2 = 0
+    
+    for i in value1:
+        num1 += name.count(i)
+    
+    for i in value2:
+        num2 += name.count(i)
+        
+    result = int(str(num1) + str(num2))
+    
+    return result
+
+love_score = love_calculator(name, value, value2)
+
+if love_score > 90:
+    print(f"Your score is {love_score} , you go together like coke and mentos.")
+    
+elif love_score > 40 and love_score < 50:
+    print(f"Your score is {love_score} , you are alright together.")
+
+else:
+    print(f"Your score is {love_score}.")
+    
+
+
+    
+
